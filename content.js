5f75ee36065f95e46c2d2fe937b57225564afe19
Itashin <itashin.0270@gmail.com>
2026-03-15 12:44:21 +0900

    Update content.js

--- content.js
+++ content.js
@@ -62,8 +62,10 @@ const siteContent = {
     date: "2026.03.15",
     tag: "Media",
     title: "「にいがた速報」様にてご紹介いただきました",
-    content: `新潟の地域情報サイト「にいがた速報」様にて、珈琲オルテガのオープン情報を記事にしていただきました。ぜひご覧ください。\n\n▼掲載記事はこちら\nhttps://gata21.jp/archives/96842261.html`,
-    image: "" 
+    content: `新潟の地域情報サイト「にいがた速報」様にて、珈琲オルテガのオープン情報を記事にしていただきました。ぜひご覧ください。\n\n▼掲載記事はこちら
+<a href="https://gata21.jp/archives/96842261.html（実際の記事のURL）" target="_blank" rel="noopener noreferrer" class="text-stone-300 underline underline-offset-4 hover:text-white transition-colors">
+</a>`,
+    image: ""
 },
             {
                 date: "2026.03.01",
